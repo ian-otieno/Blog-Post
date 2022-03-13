12da873b1923a2ae5d68ccf0347cd03c3b6687e9
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-13 10:43:26 +0300

    add init.py contents

--- manage.py
+++ manage.py
@@ -1,4 +1,30 @@
-from app import app
+from app import create_app ,db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import Quote, User, Post,Comment, Subscriber
+
+
+app = create_app('production')
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    '''
+    run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+# creating python shell
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, Quote = Quote, User=User, Post=Post,Comment=Comment, Subscriber=Subscriber)
+
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    manager.run()
