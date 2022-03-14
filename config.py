5b6a78b9f0db5a4cabaff989e26ed471440c8b28
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-14 11:44:20 +0300

    commit changes

--- config.py
+++ config.py
@@ -16,9 +16,11 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+   if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+ #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)   
+       
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:moringa@localhost/blog_test'
